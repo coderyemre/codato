018366c1bf51592f1dcb83fbd48ebce45abff30d
coderyemre <yemresofuoglu@gmail.com>
2022-11-28 12:19:15 +0300

    Download Function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+//Open Function
 var inputElement = document.getElementById("document");
 var textarea=document.getElementById("text");
-
 inputElement.addEventListener("change", () => {
   const [file] = inputElement.files;
   if (file) {
@@ -12,7 +12,42 @@ inputElement.addEventListener("change", () => {
   }
 })
 
-
-
+//Download Function
+function download() {
+  var t=document.getElementById("text").value;
+  const file = new File([t], 'new_file.txt', {
+  type: 'text/plain',});
+  const link = document.createElement('a');
+  const url = URL.createObjectURL(file);
+  link.href = url;
+  link.download = file.name;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  window.URL.revokeObjectURL(url);
+}
+//Save Function
+//Set cookie and Get cookie (!THANKS W3SCHOOLS!)
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 
 

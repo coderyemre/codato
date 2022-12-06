db86b8d920628fc0564e8eb77cc7b2d18fc91c50
coderyemre <yemresofuoglu@gmail.com>
2022-12-06 21:25:10 +0300

    Load Function Complete, Added Saves part, Upgrade Open Function, Main Saves Design and others ...

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-console.log(localStorage);
-console.log(localStorage.getItem("saves"));
-for( var i=0; i<=localStorage.length;i++){
-console.log(localStorage.key(i))
-}
 if(localStorage.getItem("saves")!=null){
     var saves_list=localStorage.saves.split(",");
-    console.log(saves_list);
+    saves_list=saves_list.filter(function(i){return i!=''});
+}
+//Load File Function
+for (var i=0;i<saves_list.length;i++){
+        var tr=document.createElement("tr");
+        document.getElementById("saves_list").appendChild(tr);
+        var td=document.createElement("td");
+        tr.appendChild(td);
+        td.addEventListener("click",function(){
+            textarea.value=localStorage.getItem(this.innerHTML);
+            file_name.value=this.innerHTML;
+            quit();
+        });
+        td.innerHTML=saves_list[i];  
 }
 var file_name=document.getElementById("file_name");
 //Open Function
@@ -13,11 +21,13 @@ var inputElement = document.getElementById("document");
 var textarea=document.getElementById("text");
 
 inputElement.addEventListener("change", () => {
-  const [file] = inputElement.files;
-  if (file) {
+    const [file] = inputElement.files;
+  if (file) { 
     const reader = new FileReader();
     reader.addEventListener("load", () => {
-      textarea.innerText = reader.result;
+      textarea.value="";
+      textarea.value = reader.result;
+      file_name.value= file.name;
     });
     reader.readAsText(file);
   }
@@ -25,18 +35,16 @@ inputElement.addEventListener("change", () => {
 
 //Download Function
 function download() {
-  var t=document.getElementById("text").value;
-  console.log(file_name);
-  const file = new File([t], file_name.value, {
-  type: 'text/plain',});
-  const link = document.createElement('a');
-  const url = URL.createObjectURL(file);
-  link.href = url;
-  link.download = file.name;
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-  window.URL.revokeObjectURL(url);
+      const file = new File([textarea.value], file_name.value, {
+      type: 'text/plain',});
+      const link = document.createElement('a');
+      const url = URL.createObjectURL(file);
+      link.href = url;
+      link.download = file.name;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
 }
 //Save Function (! THANKS alexpietsch !)
 function save(){
@@ -46,15 +54,17 @@ function save(){
     }
     else{
         localStorage.setItem(file_name.value, textarea.value);
-        //DELETE ME!!!!!
         localStorage.saves=localStorage.saves+file_name.value+",";
-        console.log(localStorage.getItem(file_name.value)); 
     }
 }
 //Load Function
 function load(){
-    
+    document.getElementById("saves").style.display="block";
 }
+function quit(){
+     document.getElementById("saves").style.display="none";
+}
+
 //Text Edit
 function char_edit(char){
     setTimeout(() => {
@@ -63,7 +73,6 @@ function char_edit(char){
         },0);  
 }
 function text_edit(event){
-    console.log(event.key);
     switch(event.key) {
         case '(':
             char_edit(')'); 
@@ -84,11 +93,8 @@ function text_edit(event){
             char_edit("\n");
             break;
         default:
-            console.log(event.keyCode);
             if(event.keyCode==32){
                 char_edit("\t");
             }
-    }
-    
-    
+    }   
 }
